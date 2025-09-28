740eac2694f5254860a50c08d3fed7298d2118e6
souldew <souldew2002@gmail.com>
2025-09-28 16:58:20 +0900

    isolate-pageを作成

---
+++ src/app/isolate-about/page.tsx
@@ -0,0 +1,8 @@
+import IsolatePage from "@/pages/Isolate/IsolatePage";
+import React from "react";
+
+const IsolateAbout = () => {
+  return <IsolatePage />;
+};
+
+export default IsolateAbout;
---
+++ src/pages/Isolate/IsolatePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const IsolatePage = () => {
+  return (
+    <div>
+      <p>このページはisolate-pageです</p>
+      <p>Buttonに依存しないページとして作っています。</p>
+    </div>
+  );
+};
+
+export default IsolatePage;
